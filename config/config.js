5b444867f4e968177a2facf005f40cde7a6740cd
Deff <jeff@Jeffs-MacBook-Pro.local>
2017-05-05 14:01:44 +0200

    Db config

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     ///Database
-    //mongodb://<dbuser>:<dbpassword>@ds062919.mlab.com:62919/compactdb
+    //mongodb://<dbuser>:<dbpassword>@ds062919.mlab.com:62919/compact
     urlDatabase: 'mongodb://root:root@ds029381.mlab.com:29381/compact',
     //urlDatabase : 'mongodb://localhost/test',
     ///Secret
