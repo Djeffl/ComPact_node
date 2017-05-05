4a031362654a4d18cebc8b269d8003488c9b124a
Deff <jeff@Jeffs-MacBook-Pro.local>
2017-05-05 12:06:48 +0200

    Config file mongodb update

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     ///Database
     //mongodb://<dbuser>:<dbpassword>@ds062919.mlab.com:62919/compactdb
-    urlDatabase: 'mongodb://<root>:<root>@ds029381.mlab.com:29381/compact',
+    urlDatabase: 'mongodb://root:root@ds029381.mlab.com:29381/compact',
     //urlDatabase : 'mongodb://localhost/test',
     ///Secret
     secret: 'Not a secret',
