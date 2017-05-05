8a21c8f3501ca05dfaebfe46e093a9cdcabffe29
Deff <jeff@Jeffs-MacBook-Pro.local>
2017-05-05 14:13:17 +0200

    Config db connection

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     ///Database
     //mongodb://<dbuser>:<dbpassword>@ds062919.mlab.com:62919/compact
-    //urlDatabase: 'mongodb://root:root@ds029381.mlab.com:29381/compact',
-    urlDatabase : 'mongodb://localhost/test',
+    urlDatabase: 'mongodb://root:root@ds029381.mlab.com:29381/compact',
+    //urlDatabase : 'mongodb://localhost/test',
     ///Secret
     secret: 'Not a secret',
     ///Google API key
